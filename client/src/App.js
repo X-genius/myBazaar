623d098becdb2b30b2be77294dc6a2af4575a320
Aman <55349238+X-genius@users.noreply.github.com>
2022-05-22 00:45:59 +0530

    create-react-app

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">myBazaar</a>
+      </header>
+      <main>List products</main>
+    </div>
+  );
+}
+
+export default App;
