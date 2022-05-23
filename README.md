9b7048ec2e4c41f0d2846ca6709be6f1976416d4
Aman <55349238+X-genius@users.noreply.github.com>
2022-05-23 22:44:01 +0530

    create-signin-backend-api

--- README.md
+++ README.md
@@ -83,3 +83,7 @@
     1. create user model
     2. seed sample users
     3. create user routes
+22. Create Sign in backend API
+    1. create sign in API
+    2. npm install jsonwebtoken
+    3. define generateToken
--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import data from './data.js';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import seedRouter from './routes/seedRoute.js';
 import ProductRouter from './routes/productRoute.js';
+import UserRouter from './routes/userRoute.js';
 
 dotenv.config();
 
@@ -18,9 +18,16 @@ mongoose
 
 const app = express();
 
-app.use('/api/seed', seedRouter);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+app.use('/api/seed', seedRouter);
 app.use('/api/products', ProductRouter);
+app.use('/api/users', UserRouter);
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
