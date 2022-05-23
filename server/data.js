1c8fe58239f9d34ee29c4c8d80f26a0acc70f7b9
Aman <55349238+X-genius@users.noreply.github.com>
2022-05-23 21:13:07 +0530

    create-product-model

--- server/data.js
+++ server/data.js
@@ -1,7 +1,6 @@
 const data = {
   products: [
     {
-      _id: '1',
       name: 'Nike Slim Shirt',
       slug: 'nike-slim-shirt',
       category: 'Shirts',
@@ -14,7 +13,6 @@ const data = {
       description: 'High Quality Nike Brand Shirt',
     },
     {
-      _id: '2',
       name: 'Adidas fit Shirt',
       slug: 'adidas-fit-shirt',
       category: 'Shirts',
@@ -27,7 +25,6 @@ const data = {
       description: 'High Quality Adidas Brand Shirt',
     },
     {
-      _id: '3',
       name: 'Nike Slim Pant',
       slug: 'nike-slim-pant',
       category: 'Pants',
@@ -40,7 +37,6 @@ const data = {
       description: 'High Quality Nike Brand Pant',
     },
     {
-      _id: '4',
       name: 'Adidas fit Pant',
       slug: 'adidas-fit-pant',
       category: 'Pants',
